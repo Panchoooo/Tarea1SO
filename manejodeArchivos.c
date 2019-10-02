a01b045914dbfba0d1408c220b5edf8a7421abc2
Panchoooo <56054937+Panchoooo@users.noreply.github.com>
2019-10-01 22:01:29 -0300

    Update manejodeArchivos.c

--- manejodeArchivos.c
+++ manejodeArchivos.c
@@ -6,7 +6,7 @@
 #include <fcntl.h>  // para open
 #include <unistd.h> // para rmkdir y creat
 #include <dirent.h> // para directorios
-
+#include <time.h>
 #include "manejodeArchivos.h"
 
 #define MAX 100
@@ -18,7 +18,7 @@ int crearJugadores(){
 
   buffer = (char*)malloc(MAX*sizeof(char));
   for (i=0;i<4;i++){
-    snprintf(buffer,10, "Jugador %d", i+1);
+    snprintf(buffer,10, "Jugador_%d", i+1);
     ret = mkdir(buffer,S_IRWXU);
   }
   free(buffer);
@@ -96,25 +96,182 @@ int crearMazo(){
   free(colores);
   return ret;
 }
-int imprimirMazo(){
+
+ int RobarCarta(char* nombre){
+   DIR *dir;
+   struct dirent *ent;
+   int i,j,z;
+   char ** lista,*buffer;
+   lista = (char**)malloc(108*sizeof(char*));
+   buffer = (char*)malloc(MAX*sizeof(char));
+
+   // leemos el direcorio de mazo
+   dir = opendir ("Mazo/");
+
+   //verificamos que no exista un error
+   if (dir == NULL){
+     printf("No puedo abrir el directorio");
+     exit(EXIT_FAILURE);
+   }
+   //leemos archivos en la carpeta Mazo
+   i=0;
+   while ((ent = readdir (dir)) != NULL){
+     // nos devolvera "." y ".."
+     if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
+       //imprimimos el archivo
+       lista[i]= (char*)malloc(sizeof(char)*strlen(ent->d_name)+1);
+       snprintf(lista[i],6+(int)strlen(ent->d_name), "%s",ent->d_name);
+       i++;
+     }
+   }
+   closedir (dir);
+   j = rand () % i + 0; // numero random enter 0 y 108
+   snprintf(buffer,6+(int)strlen(lista[j]), "Mazo/%s",lista[j]); //  removemos la carta de la carpeta Mazo
+   remove(buffer);
+   snprintf(buffer,11+(int)strlen(lista[j]), "%s/%s",nombre,lista[j]); // Se la añadimos al jugador respectivo
+   creat(buffer,S_IRWXU);
+   // Liberamos la memoria
+   for(z=0;z<i;z++){
+     free(lista[z]);
+   }
+   free(lista);
+   free(buffer);
+   return EXIT_SUCCESS;
+ }
+
+void EnJuego(){
+  mkdir("En_Juego",S_IRWXU);
+  RobarCarta("En_Juego");
+}
+
+int RepartirMazo(){
+  srand(time(NULL));
   DIR *dir;
   struct dirent *ent;
+  int i,j,turno,z;
+  char ** lista,*buffer;
+  lista = (char**)malloc(108*sizeof(char*));
+  buffer = (char*)malloc(MAX*sizeof(char));
+
   // leemos el direcorio de mazo
   dir = opendir ("Mazo/");
+
   //verificamos que no exista un error
   if (dir == NULL){
     printf("No puedo abrir el directorio");
     exit(EXIT_FAILURE);
   }
   //leemos archivos en la carpeta Mazo
+  i=0;
   while ((ent = readdir (dir)) != NULL){
     // nos devolvera "." y ".."
     if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
       //imprimimos el archivo
-      printf ("%s \n", ent->d_name);
+      lista[i]= (char*)malloc(sizeof(char)*strlen(ent->d_name)+1);
+      snprintf(lista[i],6+(int)strlen(ent->d_name), "%s",ent->d_name);
+      i++;
     }
   }
   closedir (dir);
+  // arreglo que se encargara de que el random no escoga numeros repetidos, ya que la carta ya fue utilizada.
+  int *Arr;
+  Arr = (int*)malloc(sizeof(int)*108);
+  z=0;
+  while(z<108){
+    Arr[z]=0;
+    z++;
+  }
+  for(turno = 1 ; turno<5 ;turno++){
+    for(i=0;i<7;i++){
+      j = rand () % 107 + 0; // numero random enter 0 y 108
+      while(Arr[j]==-1){
+        j=rand()% 107+0;
+      }
+      Arr[j]=-1;
+      snprintf(buffer,6+(int)strlen(lista[j]), "Mazo/%s",lista[j]); //  removemos la carta de la carpeta Mazo
+      remove(buffer);
+      snprintf(buffer,11+(int)strlen(lista[j]), "Jugador_%d/%s",turno,lista[j]); // Se la añadimos al jugador respectivo
+      creat(buffer,S_IRWXU);
+    }
+  }
+  // Liberamos la memoria
+  for(i=0;i<108;i++){
+    free(lista[i]);
+  }
+  free(lista);
+  free(buffer);
   return EXIT_SUCCESS;
+
 }
 
+int JugarCarta(char* nombre){
+
+  DIR *dir;
+  struct dirent *ent;
+  int i,z;
+  char ** lista,*buffer;
+  lista = (char**)malloc(108*sizeof(char*));
+  buffer = (char*)malloc(MAX*sizeof(char));
+
+  dir = opendir ("En_Juego");
+
+  //verificamos que no exista un error
+  if (dir == NULL){
+    printf("No puedo abrir el directorio");
+    exit(EXIT_FAILURE);
+  }
+  // leemos la carta actual En Juego
+  while ((ent = readdir (dir)) != NULL){
+    // nos devolvera "." y ".."
+    if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
+      //borra la carta actual en "En juego"
+      snprintf(buffer,10+(int)strlen(ent->d_name), "En_Juego/%s",ent->d_name);
+      printf("| Ultima carta revelada: %s |\n",ent->d_name);
+      remove(buffer); //en caso de poder jugar, (avance 2)
+    }
+  }
+  closedir (dir);
+
+  // leemos el direcorio de mazo
+  snprintf(buffer,2+(int)strlen(nombre), "%s/",nombre);
+  dir = opendir (buffer);
+
+  //verificamos que no exista un error
+  if (dir == NULL){
+    printf("No puedo abrir el directorio");
+    exit(EXIT_FAILURE);
+  }
+  //leemos archivos en la carpeta Mazo
+  i=0;
+  printf("Escoge una carta:\n");
+  while ((ent = readdir (dir)) != NULL){
+    // nos devolvera "." y ".."
+    if ( (strcmp(ent->d_name, ".")!=0) && (strcmp(ent->d_name, "..")!=0) ){
+      //imprimimos el archivo
+      lista[i]= (char*)malloc(sizeof(char)*strlen(ent->d_name)+1);
+      snprintf(lista[i],6+(int)strlen(ent->d_name), "%s",ent->d_name);
+      printf("%d) %s\n",i+1,lista[i]);
+      i++;
+    }
+  }
+  closedir (dir);
+
+  int eleccion;
+  scanf("%d", &eleccion);
+  snprintf(buffer,2+(int)strlen(lista[eleccion-1])+(int)strlen(nombre), "%s/%s",nombre,lista[eleccion-1]); //  removemos la carta de la carpeta del jugador respectivo
+  remove(buffer);
+
+  // Reemplaza por la nueva
+  snprintf(buffer,10+(int)strlen(lista[eleccion-1]), "En_Juego/%s",lista[eleccion-1]); // Se la añadimos al jugador respectivo
+  creat(buffer,S_IRWXU);
+
+  // Liberamos la memoria
+  for(z=0;z<i;z++){
+    free(lista[z]);
+  }
+  free(lista);
+  free(buffer);
+
+  return EXIT_SUCCESS;
+
+}
